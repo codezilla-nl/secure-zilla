ebac4a83a2ace279d1ceca87c22200b8552ea528
Jelle Kralt <jelle@jellekralt.nl>
2016-03-10 15:27:34 +0100

    Added button functionality

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ var AirQualityAnalogSensor = GrovePi.sensors.AirQualityAnalog
 var DHTDigitalSensor = GrovePi.sensors.DHTDigital
 var LightAnalogSensor = GrovePi.sensors.LightAnalog
 
+var Analog = GrovePi.sensors.base.Analog;
+var Digital = GrovePi.sensors.base.Digital;
+
 var board;
 var lcd = new LCD(1);
-
-
-
+var alarm, led;
+var alarmEnabled = false;
 
 function start() {
     board = new Board({
@@ -29,18 +31,47 @@ function start() {
             if (res) {
                 console.log('GrovePi Version :: ' + board.version())
 
+                board.pinMode(6, 'input');
+
                 var ultrasonicSensor = new UltrasonicDigitalSensor(3)
+                alarm = new Digital(4);
+                led = new Digital(2);
+                button = new Digital(6);
+
+                lcd.openSync();
+                lcd.clear();
+
                 console.log('UltrasonicDigitalSensor (start watch)')
                 ultrasonicSensor.on('change', function (res) {
-                    console.log('Ultrasonic onChange value=' + res)
 
-                    lcd.openSync();
-                    lcd.setText('Ultrasonic value ' + res)
-                    lcd.setRGB(res && res < 255 ? res : 255, 55, 55)
-                    lcd.closeSync();
+                    lcd.setText('Sensor:\n' + res)
 
+                    if (alarmEnabled) {
+                        if (res && res <= 60) {
+                            startAlarm();
+                        } else {
+                            stopAlarm();
+                        }
+                    }
+
+
+                    var oldVal = 0;
+                    var interval = setInterval(function() {
+                        var val = button.read()[0];
+
+                        if (typeof val !== 'undefined' && val !== false && val !== oldVal) {
+                            oldVal = val;
+
+                            if (val === 1) {
+                                switchAlarm();
+                            }
+
+                        }
+                    }, 200);
 
                 })
+
+
                 ultrasonicSensor.watch()
             }
         }
@@ -48,8 +79,26 @@ function start() {
     board.init();
 }
 
+function switchAlarm() {
+    alarmEnabled = !alarmEnabled;
+    console.log('Alarm', alarmEnabled);
+}
+
+function startAlarm() {
+    console.log('ALARM!');
+    led.write(1);
+    lcd.setRGB(255, 55, 55)
+}
+
+function stopAlarm() {
+    console.log('Stop!');
+    led.write(0);
+    lcd.setRGB(55, 255, 55)
+}
+
 function onExit(err) {
     console.log('ending')
+    lcd.closeSync()
     board.close()
     process.removeAllListeners()
     process.exit()
